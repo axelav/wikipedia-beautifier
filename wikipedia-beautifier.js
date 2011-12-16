6eb4ad853857f83a7250ce736860fdcec4cd5aa4
Axel Anderson <axelav@gmail.com>
2011-12-15 20:16:54 -0500

    style .infobox above inital body paragraph

--- wikipedia-beautifier.js
+++ wikipedia-beautifier.js
@@ -15,50 +15,44 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function beautifier()
-{
-    function insertCSS(url)
-    {
-        var head = document.getElementsByTagName("head")[0];         
-        var css = document.createElement("link");
-        css.type = "text/css";
-        css.rel = "stylesheet";
-        css.href = url;
-        head.appendChild(css);
-    }
+function beautifier() {
 
-    insertCSS(chrome.extension.getURL("wikipedia-beautifier.css"));
+	function insertCSS(url) {
+		var head = document.getElementsByTagName("head")[0];
+		var css = document.createElement("link");
+		css.type = "text/css";
+		css.rel = "stylesheet";
+		css.href = url;
+		head.appendChild(css);
+	}
 
-    function parentDiv(element)
-    {
-        return (!element.parentNode || element.parentElement.tagName == "DIV") ?
-            element.parentNode : parentDiv(element.parentNode);
-    }
+	insertCSS(chrome.extension.getURL("wikipedia-beautifier.css"));
 
-    function paragraphAdjuster(elements, margin)
-    {
-        for(var i = 0; i < elements.length; i++)
-        {
-            if(parentDiv(elements[i]) == bodyContent)
-            {
-                elements[i].className += (elements[i].className ? " " : "") + "hyphenate";
-                elements[i].style.textAlign = "justify";
-                elements[i].style.marginBottom = margin;
-            }
-        }
-    }
+	function parentDiv(element) {
+		return (!element.parentNode || element.parentElement.tagName == "DIV") ? element.parentNode : parentDiv(element.parentNode);
+	}
 
-    paragraphAdjuster(bodyContent.getElementsByTagName("p"), "1.0em");
-    paragraphAdjuster(bodyContent.getElementsByTagName("li"), "0.5em");
+	function paragraphAdjuster(elements, margin) {
+		for (var i = 0; i < elements.length; i++) {
+			if (parentDiv(elements[i]) == bodyContent) {
+				elements[i].className += (elements[i].className ? " " : "") + "hyphenate";
+				elements[i].style.textAlign = "justify";
+				elements[i].style.marginBottom = margin;
+			}
+		}
+	}
 
-    Hyphenator.run();
+	paragraphAdjuster(bodyContent.getElementsByTagName("p"), "1.0em");
+	paragraphAdjuster(bodyContent.getElementsByTagName("li"), "0.5em");
 
-    var refs = document.getElementsByClassName("references-column-width");
+	Hyphenator.run();
 
-    for(var i = 0; i < refs.length; i++)
-    {
-        refs[i].style["-webkit-column-width"] = "380px";
-    }
+	var refs = document.getElementsByClassName("references-column-width");
+
+	for (var i = 0; i < refs.length; i++) {
+		refs[i].style["-webkit-column-width"] = "380px";
+	}
+
 }
 
 beautifier();
